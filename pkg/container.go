b5dcacef1358e3693844a53b2be22fe1e4de266b
Brian Ketelsen <bketelsen@gmail.com>
2025-12-28 19:26:22 -0500

    fix: tests
    Signed-off-by: Brian Ketelsen <bketelsen@gmail.com>

--- pkg/container.go
+++ pkg/container.go
@@ -364,8 +364,9 @@ func SetupSystemDirectories(targetDir string) error {
 	}
 
 	// Set proper permissions for tmp directories (sticky bit + 0777)
-	_ = os.Chmod(filepath.Join(targetDir, "tmp"), 0o1777)
-	_ = os.Chmod(filepath.Join(targetDir, "var", "tmp"), 0o1777)
+	// Note: os.Chmod with 0o1777 doesn't set sticky bit in Go; must use os.ModeSticky
+	_ = os.Chmod(filepath.Join(targetDir, "tmp"), os.ModeSticky|0777)
+	_ = os.Chmod(filepath.Join(targetDir, "var", "tmp"), os.ModeSticky|0777)
 	fmt.Println("System directories created")
 	return nil
 }
